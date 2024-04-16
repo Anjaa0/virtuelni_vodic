894d8802c157fcaba3c11e2bb5280016cb409b5c
Anjaa0 <anjadjurdjevac@gmail.com>
2024-04-16 22:27:36 +0200

    New commit pt4

--- src/main/java/me/fit/model/Turista.java
+++ src/main/java/me/fit/model/Turista.java
@@ -4,21 +4,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 
 @Entity
+@NamedQueries({ 
+	@NamedQuery(name = Turista.GET_ALL_TURISTE, query = "Select t from Turista t"),
+	@NamedQuery(name = Turista.GET_TURISTA_BY_NAME, query = "Select t from Turista t where t.ime = :name") 
+	})
 public class Turista {
 
+	public static final String GET_ALL_TURISTE = "getAllTuriste";
+	public static final String GET_TURISTA_BY_NAME = "getTuristaByName";
+		
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "vodic_seq")
 	public Long id;
+	private String ime;
 	private String prezime;
 	private String email;
 	private String korisnickoIme;
 	private String sifra;
 	
-	public Turista(Long id, String prezime, String email, String korisnickoIme, String sifra) {
+	public Turista(Long id,String ime,String prezime, String email, String korisnickoIme, String sifra) {
 		super();
 		this.id = id;
+		this.ime=ime;
 		this.prezime = prezime;
 		this.email = email;
 		this.korisnickoIme = korisnickoIme;
@@ -69,6 +81,41 @@ public class Turista {
 		this.sifra = sifra;
 	}
 	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vodic other = (Vodic) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Turista [id=" + id + ", prezime=" + prezime + ", email=" + email + ", korisnickoIme=" + korisnickoIme
+				+ ", sifra=" + sifra + "]";
+	}
 	
 
 }
--- src/main/java/me/fit/service/TuristaService.java
+++ src/main/java/me/fit/service/TuristaService.java
@@ -1,10 +1,15 @@
 package me.fit.service;
 
+import java.util.List;
+
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import me.fit.enmus.TuristaStatus;
+import me.fit.exception.TuristaException;
 import me.fit.model.Turista;
+
 
 @Dependent
 public class TuristaService {
@@ -13,7 +18,30 @@ public class TuristaService {
 		private EntityManager em;
 		
 		@Transactional
-		public Turista createTurista(Turista t) {
+		public Turista dodajTuristu(Turista t) throws TuristaException {
+			
+			List<Turista>turisti=getAllTuriste();
+			
+	       if(turisti.contains(t)){
+	    	throw new TuristaException(TuristaStatus.EXISTS.getLabel());   
+	       }
+			
+			
 			return em.merge(t);
+		}
+
+		@Transactional
+		public List<Turista> getAllTuriste() {
+			List<Turista> turisti = em.createNamedQuery(Turista.GET_ALL_TURISTE, Turista.class).getResultList();
+
+			return turisti;
+		}
+		@Transactional
+		public List<Turista> getTuristaByName(String name) {
+			List<Turista>turisti = em.createNamedQuery(Turista.GET_TURISTA_BY_NAME, Turista.class)
+					.setParameter("name", name).getResultList();
+			
+			return turisti;
+
 		}
 }
